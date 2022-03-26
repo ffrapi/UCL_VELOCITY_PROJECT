5ef96f6b799109d4f8e2949047ff0e06c5ad94d0
Frances Pitsillides <70023430+ffrapi@users.noreply.github.com>
2022-03-26 20:01:24 +0000

    Create 2_MarkDup.sh

---
+++ Processing bam files/2_MarkDup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N C3.Mode.MarkDups  ##job name
+#$ -l tmem=16G #RAM
+#$ -l h_vmem=16G #enforced limit on shell memory usage
+#$ -l h_rt=1:00:00 ##wall time.
+#$ -j y  #concatenates error and output files (with prefix job1)
+#$ -t 1-3
+
+
+#Run on working directory
+cd $SGE_O_WORKDIR
+
+#Call software
+export PATH=/share/apps/java/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/java/lib:$LD_LIBRARY_PATH
+PICARD=/share/apps/genomics/picard-2.20.3/bin/picard.jar
+
+
+#Define variables
+SHAREDFOLDER=/SAN/ugi/LepGenomics
+SPECIES=C3_Aricia_agestis_FP
+REF=$SHAREDFOLDER/$SPECIES/RefGenome2/GCA_905147365.1_ilAriAges1.1_genomic.fna
+INPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_MODE
+OUTPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_MODE
+TAIL="RG.bam"
+
+#Set up ARRAY job
+#ls $INPUT/*RG.bam | awk -F "/" '{print $NF}' | awk -F "." '{print $1}' >> mode.names3
+NAME=$(sed "${SGE_TASK_ID}q;d" mode.names3)
+
+echo "java -Xmx6g -Xms6g -jar $PICARD MarkDuplicates \
+INPUT=$INPUT/${NAME}.$TAIL \
+OUTPUT=$OUTPUT/${NAME}.rmdup.bam \
+METRICS_FILE=$OUTPUT/${NAME}.dup.txt \
+REMOVE_DUPLICATES=false \
+VALIDATION_STRINGENCY=SILENT \
+CREATE_INDEX=true" >> 02b.1_MarkDup.mode.log
+
+
+time java -Xmx6g -Xms6g -jar $PICARD MarkDuplicates \
+INPUT=$INPUT/${NAME}.$TAIL \
+OUTPUT=$OUTPUT/${NAME}.rmdup.bam \
+METRICS_FILE=$OUTPUT/${NAME}.dup.txt \
+REMOVE_DUPLICATES=false \
+VALIDATION_STRINGENCY=SILENT \
+CREATE_INDEX=true
+
