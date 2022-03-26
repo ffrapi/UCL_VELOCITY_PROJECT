0b88bb6201ce7eea8db8b26b48d4a39efc65280f
Frances Pitsillides <70023430+ffrapi@users.noreply.github.com>
2022-03-26 19:59:59 +0000

    Create Mapping_MOD_BWA.sh

---
+++ Mapping/Mapping_MOD_BWA.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N C3.MODE_BWAmem  ##job name
+#$ -l tmem=16G #RAM
+#$ -l h_vmem=16G #enforced limit on memory shell usage
+#$ -l h_rt=10:00:00 ##wall time.
+#$ -j y  #concatenates error and output files (with prefix job1)
+#$ -t 1-12
+
+#run job in working directory
+cd $SGE_O_WORKDIR
+
+
+##Software
+BWA=/share/apps/genomics/bwa-0.7.17/bwa
+export PATH=/share/apps/genomics/samtools-1.9/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/genomics/samtools-1.9/lib:$LD_LIBRARY_PATH
+
+#Define variables
+SHAREDFOLDER=/SAN/ugi/LepGenomics
+SPECIES=C3_Aricia_agestis_FP
+REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_905147365.1_ilAriAges1.1_genomic.fna
+INPUT=$SHAREDFOLDER/$SPECIES/01b_AdapterRemoval_mode_unmerged
+OUTPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_MODE_FP
+TAIL1=pair1.truncated.gz
+TAIL2=pair2.truncated.gz
+
+##Define ARRAY names
+NAME=$(sed "${SGE_TASK_ID}q;d" mode.names.Feb22)
+
+
+##Map using array
+
+sample_name=`echo ${NAME1} | awk -F "." '{print $1}'`
+echo "[mapping running for] $sample_name"
+printf "\n"
+echo "time $BWA mem $REF $INPUT/${NAME}.$TAIL1 $INPUT/${NAME}.$TAIL2| samtools sort -o  $OUTPUT/${NAME}.bam" >> map_mode_FP.log
+time $BWA mem $REF $INPUT/${NAME}.$TAIL1 $INPUT/${NAME}.$TAIL2 | samtools sort -o  $OUTPUT/${NAME}.bam
